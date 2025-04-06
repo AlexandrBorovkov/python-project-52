56c7177190b70a075a030b73f327e56bd3c259c6
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2025-04-06 10:10:43 +0500

    create home page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .forms import CommentArticleForm
 
-def index(request):
-    return render(request, 'index.html')
+
+
+def index(request, *args, **kwargs):
+    form = CommentArticleForm()
+    return render(request, 'index.html', {'form': form})
