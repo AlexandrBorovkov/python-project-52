a487c770d8456418d305fa9992654cd64e7fb7ea
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2025-04-29 15:46:40 +0500

    fixing the tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'webserver', 'python-project-52-zv20.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
 
